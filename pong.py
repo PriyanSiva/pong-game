f88a618ecff6baa32a73798c8c6222acc12c74bb
PriyanSiva <priyansiva1995@gmail.com>
2019-11-22 18:14:36 +0530

    Ball Movement

--- pong.py
+++ pong.py
@@ -28,9 +28,11 @@ paddle_b.goto(350, 0)
 ball = turtle.Turtle()
 ball.speed(0)
 ball.color('white')
-ball.shape('circle')
+ball.shape('square')
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 1
+ball.dy = 1
 
 # Functions
 def paddle_a_up():
@@ -64,3 +66,15 @@ wn.onkey(paddle_b_down, 'Down')
 # Main Game Loop
 while True:
     wn.update()
+
+    # Move the Ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    if ball.ycor() < 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() > -290:
+        ball.sety(-290)
+        ball.dy *= -1
